024960b7b0b5c9d6005667a2623f87583a6e3719
Melisa Sosa <163027331+MelisaSosa07@users.noreply.github.com>
2024-08-15 19:04:36 -0300

    Update validacion.js

--- js/validacion.js
+++ js/validacion.js
@@ -8,19 +8,20 @@ function showAlertError() {
 
 //Las contraseñas deben coincidir en los dos campos//
 
+const regBtn = document.getElementById('regBtn');
 regBtn.addEventListener('click', (event) => {
 
-const regBtn = document.getElementById('regBtn').;
-const password1 = document.getElementById('password1').value;
-const password2 = document.getElementById('password2').value;
-const alertDanger = document.getElementById('alert-danger');
-const alertSuccess = document.getElementById('alert-success');
 
-if (password1 !== password2) {
-    //Prevenir que el formulario se envíe//
-    event.preventDefault();
-    //Mostrar un mensaje de error// 
-    alert-danger.textContent = "Las contraseñas no coinciden. Por favor, inténtelo de nuevo."; } 
-else { 
-    //Si coinciden, permitir el envío del formulario//
-    alert-success.textContent = ""; }
+    const password1 = document.getElementById('password1').value;
+    const password2 = document.getElementById('password2').value;
+    const alertDanger = document.getElementById('alert-danger');
+    const alertSuccess = document.getElementById('alert-success');
+    
+    if (password1 !== password2) {
+        //Prevenir que el formulario se envíe//
+        event.preventDefault();
+        //Mostrar un mensaje de error// 
+        alert-Danger.textContent = "Las contraseñas no coinciden. Por favor, inténtelo de nuevo."; } 
+    else { 
+        //Si coinciden, permitir el envío del formulario//
+        alert-Success.textContent = ""; }
