5e707fb385a78a9c3a4678525639f1f1a6c2f1d1
priscillafm <priscillaferreri@gmail.com>
2024-08-15 18:58:30 -0300

    Update validacion.js

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,10 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+
+if (nombre && apellido && email && password1 && password2 && password1 === password2 && terminos) {
+    showAlertSuccess();
+} else {
+    showAlertError();
+}
