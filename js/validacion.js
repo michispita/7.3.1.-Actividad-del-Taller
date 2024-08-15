2099a2bbcb583cb1dbd5e880468d2024629aca9d
Melisa Sosa <163027331+MelisaSosa07@users.noreply.github.com>
2024-08-15 18:54:00 -0300

    Update validacion.js

--- js/validacion.js
+++ js/validacion.js
@@ -5,20 +5,22 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
 //Las contraseñas deben coincidir en los dos campos//
+
+regBtn.addEventListener('click', (event) => {
+
 const regBtn = document.getElementById('regBtn').;
 const password1 = document.getElementById('password1').value;
 const password2 = document.getElementById('password2').value;
 const alertDanger = document.getElementById('alert-danger');
 const alertSuccess = document.getElementById('alert-success');
 
-
-regBtn.addEventListener('click', (event) => {
 if (password1 !== password2) {
-    // Prevenir que el formulario se envíe 
+    //Prevenir que el formulario se envíe//
     event.preventDefault();
-    // Mostrar un mensaje de error 
-    errorMessage.textContent = "Las contraseñas no coinciden. Por favor, inténtelo de nuevo."; } 
+    //Mostrar un mensaje de error// 
+    alert-danger.textContent = "Las contraseñas no coinciden. Por favor, inténtelo de nuevo."; } 
 else { 
-    // Si coinciden, permitir el envío del formulario 
-    errorMessage.textContent = ""; }
+    //Si coinciden, permitir el envío del formulario//
+    alert-success.textContent = ""; }
